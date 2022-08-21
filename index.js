03cc35420a61599af6ddcc793f69b6eb650861a8
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-08-21 05:40:04 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 49, num4 = 9;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+const mod = num3 % num4;
+const max = Math.max(12,3,13,10,20)
